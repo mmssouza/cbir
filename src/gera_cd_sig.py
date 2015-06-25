61fd1cb603842705c54dd8702f8344782316e630
Marcelo M. S. Souza <marcelo.mssouza@gmail.com>
2015-06-25 15:56:49 -0300

    cost function implementation

--- src/gera_cd_sig.py
+++ src/gera_cd_sig.py
@@ -6,6 +6,9 @@ import cPickle
 import descritores
 
 diretorio = sys.argv[1]
+bins = int(sys.argv[2])
+rmin = float(sys.argv[3])
+rmax = float(sys.argv[4])
 
 f = open(diretorio+"classes.txt","r")
 cl = cPickle.load(f)
@@ -15,10 +18,9 @@ db = {}
 
 for im_file in cl.keys():
    tmp = descritores.cd(diretorio+im_file)
-   h = np.histogram(tmp,bins = 30,range = (0.05,1.))
+   h = np.histogram(tmp,bins = 40,range = (0.1,1.))
    h = h[0].astype(float)/float(h[0].sum())
-   print im_file,h
    db[im_file] = np.hstack((cl[im_file],h))
 #   print im_file,db[im_file]
    
-cPickle.dump(db,open(sys.argv[2],"w"))
+cPickle.dump(db,open(sys.argv[5],"a"))
